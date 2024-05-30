48008534c32e62d4d96a7a26942ec3591a160864
animesh <animeshchaudhri3@gmail.com>
2024-05-31 02:01:58 +0530

    feat: get to reduce lines of code

--- new.py
+++ new.py
@@ -1,19 +1,8 @@
-import json
 import http.client
 
-def post_request():
-    url = "cheatskill.onrender.com"
-    endpoint = "/generate"
-    body = {"query": "make a fizz buzz code in python return only code no talks"}
-    json_body = json.dumps(body)
+url = "cheatskill.onrender.com"
+query = "?query=" + "addtwodigitscodeonly"
 
-    conn = http.client.HTTPSConnection(url)
-    conn.request("POST", endpoint, body=json_body, headers={"Content-Type": "application/json"})
-    response = conn.getresponse()
-
-    print("Status:", response.status)
-    print("Response:", response.read().decode())
-    conn.close()
-
-if __name__ == "__main__":
-    post_request()
+conn = http.client.HTTPSConnection(url)
+conn.request("GET", "/generate" + query)
+print(conn.getresponse().read().decode())
